46d3206c0843229ce5c6762318848a187cb3a3a9
zty37021 <32612685+zty37021@users.noreply.github.com>
2017-11-02 00:23:00 +0800

    Update ai.h

--- ai.h
+++ ai.h
@@ -1,1 +1,19 @@
 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/AISystemBase.h"
+#include "MyAISystemBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICE_API UMyAISystemBase : public UAISystemBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
